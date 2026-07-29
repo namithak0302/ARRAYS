99796c90a55fe91315aa10f0187db9f398d2710c
Namitha K <namithaknamithak7@gmail.com>
2026-07-29 12:03:43 +0530

    Time: 143 ms (8.73%), Space: 91.4 MB (8.18%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -15,10 +15,10 @@ public:
         }
         int maxi=1,count=1;
         for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
-                continue;
-            }
-            else if(nums[i]==nums[i-1]+1){
+            // if(nums[i]==nums[i-1]){
+            //     continue;
+            // }
+             if(nums[i]==nums[i-1]+1){
                 count++;
                 maxi=max(count,maxi);
                 
