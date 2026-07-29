4ab57382550439f26218c7f4331b3b064e32b961
Namitha K <namithaknamithak7@gmail.com>
2026-07-29 14:37:03 +0530

    Time: 85 ms (37.16%), Space: 88.8 MB (67.48%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,30 +2,46 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         int n=nums.size();
-        if(n==0){
-            return 0;
-        }
-        set<int> st;
+        // if(n==0){
+        //     return 0;
+        // }
+        // set<int> st;
+        // for(int i=0;i<n;i++){
+        //     st.insert(nums[i]);
+        // }
+        // int index=0;
+        // for(auto it:st){
+        //     nums[index++]=it;
+        // }
+        // int maxi=1,count=1;
+        // for(int i=1;i<n;i++){
+        //     // if(nums[i]==nums[i-1]){
+        //     //     continue;
+        //     // }
+        //      if(nums[i]==nums[i-1]+1){
+        //         count++;
+        //         maxi=max(count,maxi);
+                
+        //     }
+        //     else{
+        //         count=1;
+        //     }
+        // }
+        // return maxi;
+        unordered_set<int> st;
         for(int i=0;i<n;i++){
             st.insert(nums[i]);
         }
-        int index=0;
-        for(auto it:st){
-            nums[index++]=it;
+        int maxi=0;
+        for(auto x:st){
+        if(st.find(x-1)==st.end()){
+            int current=x;
+            int count=1;
+        while(st.find(current+1)!=st.end()){
+            count++;
+            current++;}
+            maxi=max(count,maxi);
         }
-        int maxi=1,count=1;
-        for(int i=1;i<n;i++){
-            // if(nums[i]==nums[i-1]){
-            //     continue;
-            // }
-             if(nums[i]==nums[i-1]+1){
-                count++;
-                maxi=max(count,maxi);
-                
-            }
-            else{
-                count=1;
-            }
         }
         return maxi;
     }
