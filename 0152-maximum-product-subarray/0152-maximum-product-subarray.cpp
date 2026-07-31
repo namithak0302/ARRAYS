a521fefa0e53e046707f0d0e9e5d4b21750bf089
Namitha K <namithaknamithak7@gmail.com>
2026-07-31 12:14:53 +0530

    Time: 0 ms (100%), Space: 17.8 MB (60.9%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,44 +1,24 @@
-// class Solution {
-// public:
-//     int maxProduct(vector<int>& nums) {
-//        int n=nums.size();
-        
-//     }
-// };
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-
-        int n = nums.size();
-
-        int prefix = 1;
-        int suffix = 1;
-
-        int ans = INT_MIN;
-
-        // Left to right
-        for(int i = 0; i < n; i++) {
-
-            if(prefix == 0)
-                prefix = 1;
-
-            prefix *= nums[i];
-
-            ans = max(ans, prefix);
+       int n=nums.size();
+        int prefix=1;
+        int suffix=1;
+        int ans=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(prefix==0){
+                prefix=1;
+            }
+            prefix*=nums[i];
+            ans=max(ans,prefix);
         }
-
-
-        // Right to left
-        for(int i = n - 1; i >= 0; i--) {
-
-            if(suffix == 0)
-                suffix = 1;
-
-            suffix *= nums[i];
-
-            ans = max(ans, suffix);
+         for(int i=n-1;i>=0;i--){
+            if(suffix==0){
+                suffix=1;
+            }
+            suffix*=nums[i];
+            ans=max(ans,suffix);
         }
-
-        return ans;
+return ans;
     }
 };
