69894f1b654187d60aa499ba5dcf546e6270ac2c
Namitha K <namithaknamithak7@gmail.com>
2026-08-04 09:48:40 +0530

    Time: 0 ms (100%), Space: 23.9 MB (40.43%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,30 +2,43 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
-        
+        int candidate1=0,count1=0,candidate2=0,count2=0;
+        for(int i=0;i<n;i++){
+           
+             if(nums[i]==candidate1){
+                count1++;
+            }
+            else if(nums[i]==candidate2){
+                count2++;}
+              else if(count1==0){
+                candidate1=nums[i];
+                count1=1;
+            }
+            else if(count2==0){
+                candidate2=nums[i];
+                count2=1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        count1=0;
+        count2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==candidate1){
+                count1++;
+            }
+            else if(nums[i]==candidate2){
+                count2++;
+            }
+        }
         vector<int> ans;
-        sort(nums.begin(),nums.end());
-        if(n==1){
-            return nums;
+        if(count1>n/3){
+            ans.push_back(candidate1);
         }
-
-        else{
-            int majority=nums[0],count =1;
-            for(int i=1;i<n;i++){
-                if(nums[i]==majority){
-                    count++;
-                }
-                else{
-                    if(count>n/3){
-            ans.push_back(majority);}
-            majority=nums[i];
-            count=1;
-                }
-            }
-            if(count>n/3){
-            ans.push_back(majority);}
-            }
-
-            return ans;
+        if(count2>n/3){
+            ans.push_back(candidate2);
         }
-    };
+        return ans;}
+};
