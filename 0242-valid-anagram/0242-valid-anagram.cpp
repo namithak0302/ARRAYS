f339d5da533578f7d0ebc707e80cd3ebadfc2480
Namitha K <namithaknamithak7@gmail.com>
2026-08-06 11:47:46 +0530

    Time: 0 ms (100%), Space: 9.7 MB (50.1%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n=s.length();
-        int m=t.length();
-        int count=0;
-        vector<int> freq(26, 0);
-         vector<int> freq1(26, 0);
-        if(n!=m){
-            return false;
-        }
+//         int n=s.length();
+//         int m=t.length();
+//         vector<int> freq(26, 0);
+//          vector<int> freq1(26, 0);
+//         if(n!=m){
+//             return false;
+//         }
         
-            for(int j = 0; j < n; j++)
-             {
-                freq[s[j] - 'a']++;
-    }
+//             for(int j = 0; j < n; j++)
+//              {
+//                 freq[s[j] - 'a']++;
+//     }
    
-            for(int j = 0; j < m; j++)
-             {
-                freq1[t[j] - 'a']++;
-    }
-   for(int i=0;i<26;i++){
-    if(freq[i]!=freq1[i])
+//             for(int j = 0; j < m; j++)
+//              {
+//                 freq1[t[j] - 'a']++;
+//     }
+//    for(int i=0;i<26;i++){
+//     if(freq[i]!=freq1[i])
+//     return false;
+//    }
+//    return true;
+//     }
+// };
+if(s.length()!=t.length()){
     return false;
-   }
-   return true;
-    }
+}
+vector<int> ans(26,0);
+for(char ch:s){
+    ans[ch-'a']++;
+}
+for(char ch:t){
+    ans[ch-'a']--;
+}
+for(int x:ans){
+    if(x!=0){
+        return false;
+    }}
+    return true;
+}
 };
